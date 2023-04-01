f24c3845b25c723eed71455e1f895b0e1e6ef9f9
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-04-02 02:40:51 +0530

    added the code for header home and footer section

---
+++ myapp/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+ <footer>
+<div>
+    <h1>Guru.</h1>
+    <p>@All rights reserved</p>
+</div>
+
+<div>
+    <h5>Follow me</h5>
+    <div>
+        <a href="https://youtube.com" target={"blank"}>Youtube</a>
+        <a href="https://instagram.com" target={"blank"}>instagram</a>
+        <a href="https://github.com" target={"blank"}>Github</a>
+    </div>
+</div>
+ </footer>
+    
+  )
+}
+
+export default Footer
